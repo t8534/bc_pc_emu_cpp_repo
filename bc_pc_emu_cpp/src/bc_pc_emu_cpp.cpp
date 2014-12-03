243d5de9289b71ca73c6a350a71b58458247dd16
liberdaa <liberdaa@CZEPL20213.ad.trw.com>
2014-12-03 13:01:11 +0100

    [master] Init. Compilable. Ready for tag BC00.00.01

---
+++ bc_pc_emu_cpp/src/bc_pc_emu_cpp.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : bc_pc_emu_cpp.cpp
+// Author      : .
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	std::cout << "cout test " << std::endl;
+
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
